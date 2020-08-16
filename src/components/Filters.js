a81dbcd26d11dd3d7aedd218b1952be3c88c9eee
muthu-pal <muthupal@g.ucla.edu>
2020-08-16 15:15:15 -0700

    fixed

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -27,21 +27,24 @@ let fakeServerData = {
 
 const FiltersDrop = (props) => {
   const [loggedInQuery, setLoggedInQuery] = useState("");
+  
 
   function logIn(){
     const parsed = queryString.parse(window.location.search);
+    let access_token = parsed.access_token;
     console.log(parsed);
-    setLoggedInQuery(parsed.access_token);
+    setLoggedInQuery(access_token);
+    if (parsed.grant_type === "authorization_code"){
     fetch('https://api.spotify.com/v1/me', 
-    {headers: {'Authorization': 'Bearer ' + parsed.access_token}
+    {headers: {'Authorization': 'Bearer ' + access_token}
     }).then((response) => 
       response.json()
     ).then((data) => console.log(data))
-  }
+  }}
 
   console.log(loggedInQuery)
 
-
+  
 
 
   const [dropdownOpenMood, setOpenMood] = useState(false);
@@ -116,83 +119,80 @@ const FiltersDrop = (props) => {
 
   return (
     <div>
+              <div>
+                <ButtonDropdown
+                  isOpen={dropdownOpenMood}
+                  toggle={toggleMood}
+                  className="Mood--Btn"
+                >
+                  <DropdownToggle
+                    caret
+                    style={{
+                      backgroundColor: "#1DB954",
+                      borderColor: "white",
+                      color: "white",
+                      borderRadius: "20px"
+                    }}
+                  >
+                    Mood: {Mood}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={clickedHighEnergy}>High Energy</DropdownItem>
+                    <DropdownItem onClick={clickedDanceable}>Danceable</DropdownItem>
+                    <DropdownItem onClick={clickedInstrumental}>
+                      Instrumental
+                    </DropdownItem>
+                    <DropdownItem onClick={clickedAcoustic}>Acoustic</DropdownItem>
+                    <DropdownItem onClick={clickedLive}>Live</DropdownItem>
+                    <DropdownItem onClick={clickedSpeechy}>Speechy</DropdownItem>
+                    <DropdownItem onClick={clickedChill}>Chill</DropdownItem>
+                    <DropdownItem onClick={clickedHappy}>Happy</DropdownItem>
+                    <DropdownItem onClick={clickedSad}>Sad</DropdownItem>
+                    <DropdownItem onClick={clickedPoppy}>Poppy</DropdownItem>
+                    <DropdownItem onClick={clickedIndie}>Indie</DropdownItem>
+                    <DropdownItem onClick={clickedElectronic}>Electronic</DropdownItem>
+                  </DropdownMenu>
+                </ButtonDropdown>
 
-    {(loggedInQuery !== "") ? 
-      <div>
-        <ButtonDropdown
-          isOpen={dropdownOpenMood}
-          toggle={toggleMood}
-          className="Mood--Btn"
-        >
-          <DropdownToggle
-            caret
-            style={{
-              backgroundColor: "#1DB954",
-              borderColor: "white",
-              color: "white",
-              borderRadius: "20px"
-            }}
-          >
-            Mood: {Mood}
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem onClick={clickedHighEnergy}>High Energy</DropdownItem>
-            <DropdownItem onClick={clickedDanceable}>Danceable</DropdownItem>
-            <DropdownItem onClick={clickedInstrumental}>
-              Instrumental
-            </DropdownItem>
-            <DropdownItem onClick={clickedAcoustic}>Acoustic</DropdownItem>
-            <DropdownItem onClick={clickedLive}>Live</DropdownItem>
-            <DropdownItem onClick={clickedSpeechy}>Speechy</DropdownItem>
-            <DropdownItem onClick={clickedChill}>Chill</DropdownItem>
-            <DropdownItem onClick={clickedHappy}>Happy</DropdownItem>
-            <DropdownItem onClick={clickedSad}>Sad</DropdownItem>
-            <DropdownItem onClick={clickedPoppy}>Poppy</DropdownItem>
-            <DropdownItem onClick={clickedIndie}>Indie</DropdownItem>
-            <DropdownItem onClick={clickedElectronic}>Electronic</DropdownItem>
-          </DropdownMenu>
-        </ButtonDropdown>
-
-        <ButtonDropdown
-          isOpen={dropdownOpenView}
-          toggle={toggleV}
-          className="view--Btn"
-        >
-          <DropdownToggle
-            caret
-            style={{
-              backgroundColor: "transparent",
-              borderColor: "white",
-              color: "white",
-              borderRadius: "20px"
-            }}
-          >
-            {viewNum}
-          </DropdownToggle>
-          <DropdownMenu>
-            <DropdownItem onClick={clicked10}>1</DropdownItem>
-            <DropdownItem onClick={clicked25}>2</DropdownItem>
-            <DropdownItem onClick={clicked50}>3</DropdownItem>
-          </DropdownMenu>
-        </ButtonDropdown>
-     
-      <br />
-      <br />
-    
-      {filteredStuff.map((element) => {
-        return (
-          <>
-            <Playlist
-              name={element.name}
-              artist={element.songArtist}
-              photo={"/images/Harry-Styles.jpg"}
-              photoAlt="Harry"
-            />
-          </>
-        );
-      })}
-      </div>
-      
+                <ButtonDropdown
+                  isOpen={dropdownOpenView}
+                  toggle={toggleV}
+                  className="view--Btn"
+                >
+                  <DropdownToggle
+                    caret
+                    style={{
+                      backgroundColor: "transparent",
+                      borderColor: "white",
+                      color: "white",
+                      borderRadius: "20px"
+                    }}
+                  >
+                    {viewNum}
+                  </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={clicked10}>1</DropdownItem>
+                    <DropdownItem onClick={clicked25}>2</DropdownItem>
+                    <DropdownItem onClick={clicked50}>3</DropdownItem>
+                  </DropdownMenu>
+                </ButtonDropdown>
+            
+              <br />
+              <br />
+            
+              {filteredStuff.map((element) => {
+                return (
+                  <>
+                    <Playlist
+                      name={element.name}
+                      artist={element.songArtist}
+                      photo={"/images/Harry-Styles.jpg"}
+                      photoAlt="Harry"
+                    />
+                  </>
+                );
+              })}
+        </div>
       
       : 
       (<div><h1>please sign in <Button onClick={logIn}>here</Button></h1></div>)}
