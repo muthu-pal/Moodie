8142c5aa7e6fa115b51299918ffb725fc2aaa05c
muthu-pal <muthupal@g.ucla.edu>
2020-08-19 16:47:00 -0700

    changed text to stick

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,30 +1,24 @@
 import React from "react";
 import { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import "./Playlist.css";
+import Card from 'react-bootstrap/Card'
+import {Button} from 'react-bootstrap'
 import "bootstrap/dist/css/bootstrap.min.css";
+
 
 class Playlist extends Component {
   render() {
     return (
-      <div style={{ color: "white" }}>
-        <Container className="container">
-          <Row>
-            <Col>
+      <div className="container">
+      <div style={{ color: "white" }} className="comp">
+        
               <img src={this.props.photo} alt={this.props.photoAlt} className="photo"/>
-            </Col>
-            <Col>
-              <Row> 
+            
                 <h3 className="track">{this.props.name}</h3>
-              </Row>
-              <Row>
-                <ul className="artist">{this.props.artist}</ul>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+              
+                <h3 className="artist">{this.props.artist}</h3>
+              
+      </div>
       </div>
     );
   }
